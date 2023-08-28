1a13152f2b23d5770405b21328396c767cc075ec
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2023-08-28 12:55:55 +0530

    Navbar Optimized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,32 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import { useState } from 'react';
 import '../App.css'
+import { NavLinks } from '../utils/NavLinks';
 function Navbar() {
   const [activeLink, setActiveLink] = useState('');
+
+  const handleLinkClick = link => {
+    setActiveLink(link);
+  };
+
   return (
     <div className='h-[80px] px-10 flex flex-row justify-between items-center'>
-        <Link to='/' className={`nav-link ${activeLink === 'home' ? 'active' : ''}`} onClick={() => setActiveLink('home')}><img src={logo} alt="" /></Link>
-        <div className='flex flex-row gap-10 text-lg font-semibold'>
-            <Link to='/' className={`nav-link ${activeLink === 'home' ? 'active' : ''}`} onClick={() => setActiveLink('home')}>Home</Link>
-            <Link to='/about' className={`nav-link ${activeLink === 'about' ? 'active' : ''}`} onClick={() => setActiveLink('about')}>About</Link>
-            <Link to='/contact' className={`nav-link ${activeLink === 'contact' ? 'active' : ''}`} onClick={() => setActiveLink('contact')}>Contact</Link>
-            <Link to='/todo-app' className={`nav-link ${activeLink === 'todo-app' ? 'active' : ''}`} onClick={() => setActiveLink('todo-app')}>Todo-App</Link>
-            <Link to='/shopping-cart' className={`nav-link ${activeLink === 'shopping-cart' ? 'active' : ''}`} onClick={() => setActiveLink('shopping-cart')}>Shopping Cart</Link>
-            <Link to='/photos-api' className={`nav-link ${activeLink === 'photos-api' ? 'active' : ''}`} onClick={() => setActiveLink('photos-api')}>Photos API</Link>
-        </div>
+    <Link to='/' className={`nav-link ${activeLink === 'home' ? 'active' : ''}`} onClick={() => handleLinkClick('home')}>
+      <img src={logo} alt="" />
+    </Link>
+    <div className='flex flex-row gap-10 text-lg font-semibold'>
+      {NavLinks.map((navLink, index) => (
+        <Link
+          key={index}
+          to={navLink.to}
+          className={`nav-link ${activeLink === navLink.text.toLowerCase() ? 'active' : ''}`}
+          onClick={() => handleLinkClick(navLink.text.toLowerCase())}
+        >
+          {navLink.text}
+        </Link>
+      ))}
     </div>
+  </div>
   )
 }
 
---
+++ src/utils/NavLinks.js
@@ -0,0 +1,8 @@
+export const NavLinks = [
+  { to: '/', text: 'Home' },
+  { to: '/about', text: 'About' },
+  { to: '/contact', text: 'Contact' },
+  { to: '/todo-app', text: 'Todo-App' },
+  { to: '/shopping-cart', text: 'Shopping Cart' },
+  { to: '/photos-api', text: 'Photos API' },
+];
